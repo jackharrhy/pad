d99b8ec87a26fe16514d2bfca611448a5c838059
Jack Arthur Harrhy <me@jackharrhy.com>
2021-06-21 01:47:08 -0230

    wip

--- src/pad.cr
+++ src/pad.cr
@@ -2,14 +2,16 @@ require "kemal"
 require "ecr"
 require "json"
 
-PAD_DIR = "./data"
-PAD_FILE = "pad.txt"
-PAD = "#{PAD_DIR}/#{PAD_FILE}"
+require "sqlite3"
 
-unless Dir.exists?(PAD_DIR) && File.exists?(PAD)
-  Dir.mkdir_p(PAD_DIR)
-  File.write(PAD, "")
+DATA_DIR  = "./data"
+DATA_FILE = "#{DATA_DIR}/pad.db"
+
+unless Dir.exists?(DATA_DIR)
+  Dir.mkdir_p(DATA_DIR)
 end
+
+db = DB.open("sqlite3://#{DATA_FILE}")
 
 def should_return_html(env)
   headers = env.request.headers
@@ -24,16 +26,29 @@ get "/" do |env|
     env.response.content_type = "text/html"
     ECR.render("./src/site.ecr")
   else
+    # check for secret header, 400 otherwise
+
+    # lookup secret header in database, create if not existing yet
+
+    # return data if all gucci
+
     env.response.content_type = "text/plain"
-    File.read(PAD)
   end
 end
 
 put "/" do |env|
+  # check for nil body and handle better
   text = env.request.body.not_nil!.gets_to_end
 
+  # check for secret header, 400 otherwise
+
+  # lookup secret header in database, if not exist return empty string
+
+  # return data
+
   env.response.status_code = 204
-  File.write(PAD, text)
+
+  ""
 end
 
 Kemal.run
